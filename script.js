f57a478650b6f893775bf3f05153c27b027551cb
Rishab-Rawat <rawatrishab66@gmail.com>
2024-07-23 20:07:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', (event) => {
+    const inputs = document.querySelectorAll('.code');
+    
+    inputs.forEach((input, index) => {
+        input.addEventListener('keydown', (e) => {
+            if (e.key >= 0 && e.key <= 9) {
+                inputs[index].value = '';
+                setTimeout(() => {
+                    if (index < inputs.length - 1) {
+                        inputs[index + 1].focus();
+                    }
+                }, 10);
+            } else if (e.key === 'Backspace') {
+                if (index > 0 && !inputs[index].value) {
+                    setTimeout(() => inputs[index - 1].focus(), 10);
+                }
+            } else {
+                e.preventDefault();
+            }
+        });
+    });
+});
